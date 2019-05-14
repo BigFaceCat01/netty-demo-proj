5c3f02b4084447c10f1bf893ab6f946e3a8bdc5a
huangxb <18228033207@163.com>
2019-05-14 10:04:29 +0800

    RPC已移动到yma-rpc仓库下

--- src/main/java/com/hxb/smart/rpc/base/Resource.java
+++
@@ -1,9 +0,0 @@
-package com.hxb.smart.rpc.base;
-
-/**
- * @author Created by huang xiao bao
- * @date 2019-05-10 17:57:17
- */
-public interface Resource {
-    void read(String configPath);
-}
--- src/test/RpcV2Test.java
+++ src/test/RpcV2Test.java
@@ -1,11 +1,3 @@
-import com.hxb.smart.rpc.api.HelloService;
-import com.hxb.smart.rpcv2.RpcFactory;
-import com.hxb.smart.rpcv2.configuration.RpcConfig;
-import com.hxb.smart.rpcv2.core.invoker.reference.InvokerProxyBean;
-import com.hxb.smart.rpcv2.core.invoker.router.impl.RandomRouter;
-import com.hxb.smart.rpcv2.core.net.NetType;
-import com.hxb.smart.rpcv2.registry.impl.LocalServiceRegistry;
-import com.hxb.smart.rpcv2.serializer.impl.HessianSerializer;
 import org.junit.Test;
 
 /**
@@ -15,17 +7,8 @@ import org.junit.Test;
 public class RpcV2Test {
 
     @Test
-    public void testRpc(){
-        RpcConfig config = RpcConfig.builder()
-                .basePackage("com.hxb")
-                .netType(NetType.NETTY)
-                .port(9090)
-                .registryCenterAddress("0.0.0.0")
-                .router(new RandomRouter())
-                .serviceRegistry(new LocalServiceRegistry())
-                .serializer(new HessianSerializer())
-                .build();
-        RpcFactory.builder().config(config).build();
+    public void test(){
+
         
     }
 }
